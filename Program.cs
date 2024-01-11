e2e2e9d655d4222fe44ea4ee1e2dc917b9a268a9
Fabio Carvalho <bio1809@gmail.com>
2024-01-11 18:16:38 -0300

    Desafio - Abstraindo um Celular com POO Concluido!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "987540000", modelo: "LumiaZYX", imei: "1234567890", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp Business");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "987540000", modelo: "LumiaZYX", imei: "1234567890", memoria: 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
